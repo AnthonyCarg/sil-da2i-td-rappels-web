29895e943a87d8873c63ccf9526ccbe9c6ece156
AnthonyCarg <anthony.cargnino@gmail.com>
2017-12-05 10:48:09 +0100

    TD 3 étape 2

--- Vue/footer.php
+++ Vue/footer.php
@@ -1,26 +1,119 @@
-<?php
-echo '<aside>';
-echo '<h1>Glossaire</h1>';
-echo '<dl>';
-echo '<dt>Lorem</dt>';
-echo '<dd>ipsum dolor sit amet</dd>';
-echo '</dl>';
-echo '</aside>';
-echo '</section>';
-echo '</main>';
-echo '<footer>';
-		echo '<address>Auteur: Anthony Cargnino</address>';
-	    echo '<script>';
-        echo '$(document).ready(function() {';
-		echo '$(\'aside:first\')';
-			echo '.hide(2000)';
-        echo '});';
-		echo 'setTimeout(function(){ $(\'.navbar\').slideUp(\'slow\') }, 500);';
-		echo 'setTimeout(function(){ $(\'.navbar\').slideDown(\'slow\') }, 1000);';
-		echo 'setTimeout(function(){ $(\'img\').fadeOut(\'slow\') }, 2500)';
-    echo '</script>';
-echo '</footer>';
-echo '</main>';	
-echo '</body>';
-echo'</html>';
-?>
+<aside>
+<h1 id="faq">FAQ</h1>
+<dl>
+	<dt>Question 1</dt>
+		<dd>
+			<dl>
+				<dt>réponse 1 partie 1</dt>
+				<dd>détail 1</dd>
+				<dt>réponse 1 partie 2</dt>
+				<dd>détail 2</dd>
+			</dl>
+		</dd>
+	<dt>Question 2</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 3</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 4</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 5</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 6</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 7</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 8</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 9</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+	<dt>Question 10</dt>
+	<dd>
+		<dl>
+			<dt>réponse 1 partie 1</dt>
+			<dd>détail 1</dd>
+			<dt>réponse 1 partie 2</dt>
+			<dd>détail 2</dd>
+		</dl>
+	</dd>
+</dl>
+</aside>
+</section>
+</main>
+<footer>
+	<address>Auteur: Anthony Cargnino</address>
+	<script>
+        $('#hideAside').on('click', function() {
+			$('aside:first').hide(2000)
+        });
+		$('#fadeImg').on('click', function() {
+			setTimeout(function(){ $('img').fadeOut('slow') }, 2500)
+		});
+		$('#toggleMenu').on('click', function() {
+			setTimeout(function(){ $('.navbar').slideToggle('slow') }, 500)
+		});
+		$( "dd" ).click(function(e) {
+			e.stopPropagation();
+			alert($(this).text())
+		});
+    </script>
+</footer>
+</main>
+</body>
+</html>
+
--- Vue/header.php
+++ Vue/header.php
@@ -1,5 +1,4 @@
-<?php
-echo '<!doctype html>
+<!doctype html>
 <html class="no-js" lang="en">
 	<head>
 	<meta charset="UTF-8">
@@ -10,16 +9,19 @@ echo '<!doctype html>
     crossorigin="anonymous"></script>
 </head>
 <body>
-<main>';
+<main>
+<?php
 	require 'DB.php'; // J'inclus la classe.
 	$db = new DB; 
 	
 	$connect = $db->connect();
-echo '<header>
+?>
+	<header>
 	<ul class="navbar">
 		<li class="dropdown">
 			<a href="javascript:void(0)" class="dropbtn">Films</a>
-				<div class="dropdown-content">';
+				<div class="dropdown-content">
+				<?php
 					$sql = 'SELECT titre FROM `film` order by titre';
 					$prepare = $db->prepare($sql);
 					$query = $db->execute();
@@ -27,11 +29,13 @@ echo '<header>
 					for ($i = 0; $i < count($fetch); $i++) {
 						print('<a href="film.php">' . $fetch[$i]['titre']  . '</a>');
 					}
-				echo '</div>';
-			  echo '</li>';
-			  echo '<li class="dropdown">';
-				echo '<a href="javascript:void(0)" class="dropbtn">Réalisateurs</a>';
-				echo '<div class="dropdown-content">';
+				?>
+				</div>
+				</li>
+				<li class="dropdown">
+				<a href="javascript:void(0)" class="dropbtn">Réalisateurs</a>
+				<div class="dropdown-content">
+				<?php
 					$sql = 'SELECT personne.nom, personne.prenom FROM `film_has_personne` left join `personne` on personne.id = film_has_personne.id_personne WHERE film_has_personne.role = \'Realisateur\' order by personne.nom, personne.prenom';
 					$prepare = $db->prepare($sql);
 					$query = $db->execute();
@@ -39,11 +43,13 @@ echo '<header>
 					for ($i = 0; $i < count($fetch); $i++) {
 						print('<a href="realisateur.php">' . $fetch[$i]['prenom'] . ' ' . $fetch[$i]['nom'] . '</a>');
 					}
-				echo '</div>';
-			  echo '</li>';
-			  echo '<li class="dropdown">';
-				echo '<a href="javascript:void(0)" class="dropbtn">Acteurs</a>';
-				echo '<div class="dropdown-content">';
+				?>
+				</div>
+				</li>
+				<li class="dropdown">
+				<a href="javascript:void(0)" class="dropbtn">Acteurs</a>
+				<div class="dropdown-content">
+				<?php
 					$sql = 'SELECT personne.nom, personne.prenom FROM `film_has_personne` left join `personne` on personne.id = film_has_personne.id_personne WHERE film_has_personne.role = \'Acteur\' order by personne.nom, personne.prenom';
 					$prepare = $db->prepare($sql);
 					$query = $db->execute();
@@ -51,25 +57,25 @@ echo '<header>
 					for ($i = 0; $i < count($fetch); $i++) {
 						print('<a>' . $fetch[$i]['prenom'] . ' ' . $fetch[$i]['nom'] . '</a>');
 					}
-					//var_dump($fetch);
-				echo '</div>';
-			  echo '</li>';
-			  echo '<li class="dropdown">';
-				echo '<a href="javascript:void(0)" class="dropbtn">Gestion</a>';
-				echo '<div class="dropdown-content">';
-					echo '<a>Ajouter un film</a>';
-					echo '<a>Liste des films</a>';
-				echo '</div>';
-				echo '</li>';
-				echo '</li>';
-					echo '<li class="dropdown">';
-					echo '<a href="javascript:void(0)" class="dropbtn">Sélecteurs et effets</a>';
-					echo '<div class="dropdown-content">';
-					echo '<button id="hideAside">Cacher le Aside</button>';
-					echo '<button id="fadeImg">Fondu des images</button>';
-					echo '<button id="toggleMenu">Basculer le menu</button>';
-					echo '</div>';
-				echo '</li>';
-			echo '</ul>';
-echo '</header>';
-?>
+				?>
+				</div>
+				</li>
+					<li class="dropdown">
+					<a href="javascript:void(0)" class="dropbtn">Gestion</a>
+					<div class="dropdown-content">
+						<a>Ajouter un film</a>
+						<a>Liste des films</a>
+					</div>
+				</li>
+				</li>
+					<li class="dropdown">
+					<a href="javascript:void(0)" class="dropbtn">Sélecteurs et effets</a>
+					<div class="dropdown-content">
+						<a id="hideAside">Cacher le Aside</a>
+						<a id="fadeImg">Fondu des images</a>
+						<a id="toggleMenu">Basculer le menu</a>
+					</div>
+				</li>
+			</ul>
+	</header>
+
