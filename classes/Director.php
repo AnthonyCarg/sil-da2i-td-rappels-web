3fb8494f15fe270779b2b0c9b73fa35005a8e2c7
AnthonyCarg <anthony.cargnino@gmail.com>
2017-12-11 13:22:33 +0100

    TD 2 étape 2
    ajout de la fonction getBaseInfos

--- classes/Director.php
+++ classes/Director.php
@@ -20,6 +20,81 @@ class Director
 		}
 	}
 	
-	function getBaseInfos(){}
+	function getBaseInfos(){
+		require_once "DB.php";
+		$db = new DB; 
+		$connect = $db->connect();
+		echo '<main>
+			<section>
+					<h1>Réalisateur</h1>
+					<h2 class="photo">Présentation</h2>';
+							$sql = 'SELECT photo.chemin, photo.legende, film_has_personne.role FROM `personne_has_photo` left join `photo` on photo.id = personne_has_photo.id_photo join `film_has_personne` on film_has_personne.id_personne = personne_has_photo.id_personne where film_has_personne.role = "Realisateur" and film_has_personne.id_film = 1';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print('<figure><img class="personne" src="'.$fetch[$i]['chemin'].'" alt="'.$fetch[$i]['legende'].'"><figcaption>'.$fetch[$i]['legende'].'</figcaption></figure>');
+							}
+							echo '<p>Il est née le <time datetime="2001-05-15T19:00">';
+							$sql = 'SELECT date_de_naissance, biographie FROM personne WHERE id=1';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print($fetch[$i]['date_de_naissance']);
+								echo '</time>.</p>';
+								echo '<h2>Biographie</h2>';
+								print($fetch[$i]['biographie']);
+							}
+					echo '<h2>Filmographie</h2>
+					<h3>En tant que réalisateur:</h3>
+						<ul>
+							<li>1987 : My Best Friend\'s Birthday</li>
+							<li>1992 : Reservoir Dogs</li>
+							<li>1994 : Pulp Fiction</li>
+							<li>1995 : Groom Service</li>
+							<li>1997 : Jackie Brown</li>
+							<li>2003 : Kill Bill : Volume 1</li>
+							<li>2004 : Kill Bill : Volume 2</li>
+							<li>2005 : Sin City</li>
+							<li>2007 : Boulevard de la mort</li>
+							<li>2009 : Inglourious Basterds</li>
+							<li>2012 : Django Unchained</li>
+							<li>2015 : Les Huit Salopards</li>
+							<li>2019 : La Famille Manson</li>
+						</ul>
+					<h3>En tant qu\'acteur:</h3>
+						<ul>
+							<li>1987 : My Best Friend\'s Birthday</li>
+							<li>1992 : Reservoir Dogs</li>
+							<li>1994 : Pulp Fiction</li>
+							<li>1994 : Somebody to Love d\'Alexandre Rockwell</li>
+							<li>1994 : Sleep with Me</li>
+							<li>1995 : Destiny Turns on the Radio</li>
+							<li>1995 : Groom Service</li>
+							<li>1995 : Desperado</li>
+							<li>1996 : Girl 6</li>
+							<li>1996 : Une nuit en enfer</li>
+							<li>1997 : Jackie Brown</li>
+							<li>2000 : Little Nicky de Steven Brill</li>
+							<li>2003 : Kill Bill : Volume 1</li>
+							<li>2005 : Le Magicien d\'Oz des Muppets</li>
+							<li>2007 : Boulevard de la mort</li>
+							<li>2007 : Planète Terreur</li>
+							<li>2008 : Chronique des morts-vivants</li>
+							<li>2009 : Inglourious Basterds</li>
+							<li>2012 : Django Unchained</li>
+							<li>2014 : Broadway Therapy</li>
+							<li>2015 : Les Huit Salopards</li>
+						</ul>
+					<h2>Acteurs fétiches</h2>'; 
+						$sql = 'SELECT photo.chemin, photo.legende, film_has_personne.role FROM `personne_has_photo` left join `photo` on photo.id = personne_has_photo.id_photo join `film_has_personne` on film_has_personne.id_personne = personne_has_photo.id_personne where film_has_personne.role = "Acteur" and personne_has_photo.id_personne = 2 or personne_has_photo.id_personne = 5';
+						$prepare = $db->prepare($sql);
+						$query = $db->execute();
+						$fetch = $db->fetch($query);
+						for ($i = 0; $i < count($fetch); $i++) {
+							print('<figure><img class="personne" src="'.$fetch[$i]['chemin'].'" alt="'.$fetch[$i]['legende'].'"><figcaption>'.$fetch[$i]['legende'].'</figcaption></figure>');
+						}
+	}
 }
 ?>
--- classes/Movie.php
+++ classes/Movie.php
@@ -1,6 +1,14 @@
 <?php
 class Movie
 {	
+	public static $titre;
+	public static $date;
+	public static $actors;
+	public static $synopsis;
+	public static $note;
+	public static $image;
+	public static $director;
+
 	public function __construct() { }
 
 	static function getAllMovies() {
@@ -16,6 +24,88 @@ class Movie
 		}
 	}
 
-	function getBaseInfos(){}
+	function getBaseInfos(){
+		require_once "DB.php";
+		$db = new DB; 
+		$connect = $db->connect();
+		echo '<main>
+				<section>
+					<h1>';  
+							$sql = 'SELECT titre FROM film WHERE titre = \'Django Unchained\'';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print($fetch[$i]['titre']);
+							}
+						echo '</h1>
+					<h2>Date de sortie</h2>
+						<p>Le film est sortie le <time>';
+						    $sql = 'SELECT date_de_sortie FROM film WHERE titre = \'Django Unchained\'';
+						    $prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print($fetch[$i]['date_de_sortie']);
+							}
+						echo '</time>.</p>
+					<h2>Noms des acteurs principaux</h2>
+						<ul>';
+							
+							$sql = 'SELECT personne.nom, personne.prenom FROM `film_has_personne` left join `personne` on personne.id = film_has_personne.id_personne WHERE film_has_personne.id_film = 1 and film_has_personne.role = \'Acteur\'';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print('<li>' . $fetch[$i]['prenom'] . ' ' . $fetch[$i]['nom'] . '</li>');
+							}
+						echo '</ul>
+					<h2>Synopsis</h2>
+						<p> ';
+							$sql = 'SELECT synopsis FROM film WHERE titre = \'Django Unchained\'';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print($fetch[$i]['synopsis']);
+							}
+						echo '</p>
+					<h2>Note</h2>
+						<p>Ce film est noté <meter min="0" max="5" value="';
+							$sql = 'SELECT note FROM film WHERE titre = \'Django Unchained\'';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print($fetch[$i]['note']);
+							}
+						echo '"></meter>.</p>
+					<h2 class="image">Image du film</h2>';
+						 
+							$sql = 'SELECT photo.chemin, photo.legende FROM `film_has_photo` left join `photo` on photo.id = film_has_photo.id_photo WHERE film_has_photo.id_film = 1';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print('<figure><img class="film" src="'.$fetch[$i]['chemin'].'" alt="'.$fetch[$i]['legende'].'"><figcaption>'.$fetch[$i]['legende'].'</figcaption></figure>');
+							}
+					echo '<h2 id="photo">Réalisateur</h2>';
+						 
+							$sql = 'SELECT photo.chemin, photo.legende, film_has_personne.role FROM `personne_has_photo` left join `photo` on photo.id = personne_has_photo.id_photo join `film_has_personne` on film_has_personne.id_personne = personne_has_photo.id_personne where film_has_personne.role = "Realisateur" and film_has_personne.id_film = 1';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print('<figure><img class="personne" src="'.$fetch[$i]['chemin'].'" alt="'.$fetch[$i]['legende'].'"><figcaption>'.$fetch[$i]['legende'].'</figcaption></figure>');
+							}
+					echo '<h2 class="photo">Acteurs</h2>';
+							$sql = 'SELECT photo.chemin, photo.legende, film_has_personne.role FROM `personne_has_photo` left join `photo` on photo.id = personne_has_photo.id_photo join `film_has_personne` on film_has_personne.id_personne = personne_has_photo.id_personne where film_has_personne.role = "Acteur" and film_has_personne.id_film = 1';
+							$prepare = $db->prepare($sql);
+							$query = $db->execute();
+							$fetch = $db->fetch($query);
+							for ($i = 0; $i < count($fetch); $i++) {
+								print('<figure><img class="personne" src="'.$fetch[$i]['chemin'].'" alt="'.$fetch[$i]['legende'].'"><figcaption>'.$fetch[$i]['legende'].'</figcaption></figure>');
+							}					
+	}
 }
 ?>
